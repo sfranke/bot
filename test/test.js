e00456e83643c0ecb0a656230dd073a8abfbec11
sfranke <sfr4nke@gmail.com>
2017-06-23 19:40:49 +0200

    Initial commit.

---
+++ test/test.js
@@ -0,0 +1,25 @@
+const assert = require('assert')
+const util = require('util')
+
+describe('Testing irc-bot.', function() {
+
+  describe('The system ', function() {
+
+    it('should be able to connect to a server.')
+    it('should be able to have a nickname.')
+    it('should be able to authenticate.')
+    it('should be able to join a channel.')
+    it('should be able to leave a channel.')
+    it('should be able to send text messages.')
+    it('should be able to execute commands.')
+    it('should be able to disconnect from the server.')
+    it('should be able to recognize other users.')
+    it('should be able to recognize links.')
+    it('should be able to delete messages from other users.')
+    it('should be able to follow links.')
+    it('should be able to analyse the link\'s location (text).')
+    it('should be have a dictionary of \'bad\' words.')
+    it('should be able to validate the link\'s locations text against the dictonary.')
+  })
+
+})
