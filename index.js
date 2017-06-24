99ab2ee99cc4921404c323ca097cf5c578abb83d
sfranke <sfr4nke@gmail.com>
2017-06-24 20:47:14 +0200

    Added command line support.

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const logger = require('./logger.js')
 const config = JSON.parse(require('fs').readFileSync('config.json'))
 const util = require('util')
 
+let stdin = process.stdin;
+
+// without this, we would only get streams once enter is pressed
+// stdin.setRawMode( true );
+
+// resume stdin in the parent process (node app won't quit all by itself
+// unless an error or process.exit() happens)
+stdin.resume();
+
+// i don't want binary, do you?
+stdin.setEncoding( 'utf8' );
+
+// on any data into stdin
+stdin.on( 'data', function( key ){
+  // ctrl-c ( end of text )
+  if ( key === '\u0003' ) {
+    process.exit();
+  }
+  logger.log('debug', 'Got your input: ' + key)
+  // write the key to stdout all normal like
+  // process.stdout.write( key );
+});
+
 twitchStream.connect({
   // Connection credentials.
   user : config.user,
@@ -21,7 +44,7 @@ twitchStream.connect({
   // This one gets fired after the connection is established.
   done: function(client)
   {
-    logger.log('debug', 'done()' + util.inspect(client))
+    // logger.log('debug', 'done()' + util.inspect(client))
 
     // Join any channel.
     client.join(config.channel[0])
@@ -33,30 +56,41 @@ twitchStream.connect({
       // Here we can get access to all the information that get sent our way.
       let command = res.trailing.split('!')
       let user = res.string.split('!')
-      let channel = res.string.split('@')
+      let channelName = res.params
+      logger.log('debug', 'MyChannelname: ' + channelName)
 
+      logger.log('info', user[0] + ": in " + channelName + ': ' + res.trailing)
 
       // See if there is a '!' (exclamation mark) at the beginning.
       if (res.trailing != undefined && res.trailing.charAt(0) == '!') {
         logger.log('debug', 'Recognized a chat command!')
         // Output all strings here once for debug.
         logger.log('debug', 'Test command: ' + command[1])
-        logger.log('debug', 'Filter channel name: ' + channel[1].match(/^(.*?)\..*/g))
-
-        // Isolate the channel name.
-        let channelName = channel[1].split('.')
-        logger.log('debug', 'channelName: ' + channelName[0])
+        logger.log('debug', 'channelName: ' + channelName)
 
         // Checking for specific command string, here: 'help'.
         if (command[1] == 'help' && user[0] == ':webeplaying') {
           logger.log('debug', 'This is where the help command should be handled.')
-          client.send('#' + channelName[0], '~~~ Gotcha !help command ~~~')
+          logger.log('info', 'Sending response to chat.. ' + '~~~ Gotcha !help command ~~~')
+          client.send(channelName, '~~~ Gotcha !help command ~~~')
         }
       }
 
       // See if we can find a http link in the message sent.
-      if (res.trailing != undefined && res.trailing.match(/^https?:/g) && user[0] == ':webeplaying') {
-        logger.log('debug', 'client.on(\'data\') -> Found link!')
+      if (res.trailing != undefined && res.trailing.match(/\s?https?:\s?/g)) {
+        logger.log('debug', 'client.on(\'data\') -> Found link! ' + util.inspect(res))
+        logger.log('info', 'Found link! ' + 'Posted by: ' + user[0] + ' in: ' + channelName + '\n' + res.trailing)
+
+        // Iterating the text and check for a valid link in there.
+        let sentence = res.trailing.split(' ')
+        for (word in sentence) {
+          logger.log('debug', 'Checking each word in the sentence: ' + sentence[word])
+          if (sentence[word].match(/^https?:/g)) {
+            logger.log('debug', 'Extracted link: ' + sentence[word])
+            logger.log('info', 'Extracted link: ' + sentence[word])
+            // Now here check the link.
+          }
+        }
       }
     })
 
